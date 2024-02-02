897c87c493d303024a463d1143a0257e31692d45
sunsuking <sunsuking@gmail.com>
2024-02-02 17:46:40 +0900

    [Silver IV] Title: 카드2, Time: 148 ms, Memory: 51124 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < num; i++) {
+            queue.offer(i + 1);
+        }
+
+        while (queue.size() != 1) {
+            queue.poll();
+            int pop = queue.poll();
+            queue.offer(pop);
+        }
+        System.out.println(queue.poll());
+    }
+}
