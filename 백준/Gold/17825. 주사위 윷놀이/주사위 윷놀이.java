a63c137e1b5e510654970937357a38d0fd127ac1
sunsuking <sunsuking@gmail.com>
2024-02-02 15:20:19 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 164 ms, Memory: 19252 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
@@ -4,34 +4,48 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    private static int[] nums;
-    private static Map<Integer, Integer> dices;
+    private static int[] dices;
     private static int max = Integer.MIN_VALUE;
+    private static int[] path = {
+            0,
+            // 일반 1 ~ 25
+            2, 4, 6, 8, 0,
+            12, 14, 16, 18, 0,
+            22, 24, 26, 28, 0,
+            32, 34, 36, 38, 40,
+            0, 0, 0, 0, 0,
+            // 파란 경로 26 ~ 45
+            10, 13, 16, 19, 25,
+            30, 35, 40, 0, 0,
+            0, 0, 0, 0, 0,
+            // 파란 경로 41 ~ 55
+            20, 22, 24, 25, 30,
+            35, 40, 0, 0, 0,
+            0, 0, 0, 0, 0,
+            // 파란 경로 56 ~ 70
+            30, 28, 27, 26, 25,
+            30, 35, 40, 0, 0,
+            0, 0, 0, 0, 0
+    };
+
+    private static boolean contain(Integer[] p, int position) {
+        for (int j : p) {
+            if (position == j) return true;
+            if (path[j] == 40 && path[position] == 40) return true;
+            if (path[j] == 25 && path[position] == 25) return true;
+            if (path[j] == 30 && path[position] == 20) return true;
+            if (path[j] == 35 && path[position] == 35) return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        nums = new int[10];
+        dices = new int[10];
         for (int i = 0; i < 10; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            dices[i] = Integer.parseInt(st.nextToken());
         }
-
-        dices = new HashMap<>();
-
-        for (int i = 1; i <= 20; i++) {
-            dices.put(i, i * 2);
-        }
-        dices.put(21, 13);
-        dices.put(22, 16);
-        dices.put(23, 19);
-        dices.put(24, 22);
-        dices.put(25, 24);
-        dices.put(26, 28);
-        dices.put(27, 27);
-        dices.put(28, 26);
-        dices.put(29, 25);
-        dices.put(30, 30);
-        dices.put(31, 35);
-        dices.put(32, 40);
 
         dfs(new Integer[]{0, 0, 0, 0}, 0, 0);
         System.out.println(max);
@@ -43,63 +57,43 @@ public class Main {
             return;
         }
 
-        Set<Integer> set = new HashSet<>(Arrays.asList(p).subList(0, 4));
+        Set<Integer> set = new HashSet<>(Arrays.asList(p).subList(0, p.length));
         for (int i : set) {
             int index = indexOf(p, i);
-            int newPosition = calculatePosition(p[index], nums[depth]);
-            if ((newPosition <= 32 && set.contains(newPosition)) || (newPosition == 32 && set.contains(20)) || (newPosition == 20 && set.contains(32))) continue;
-            Integer[] newP = Arrays.copyOf(p, 4);
-            int newScore = score;
-            if (newPosition <= 32) {
-                newScore += dices.get(newPosition);
+            int newPosition = calculatePosition(p[index], dices[depth]);
+            if (newPosition != -1 && contain(p, newPosition)) continue;
+            int newScore = 0;
+            Integer[] newP;
+            if (newPosition == -1) {
+                newP = new Integer[p.length - 1];
+                int k = 0;
+                for (int x = 0; x < p.length; x++) {
+                    if (x != index) {
+                        newP[k++] = p[x];
+                    }
+                }
+            } else {
+                newScore = path[newPosition];
+                newP = Arrays.copyOf(p, p.length);
+                newP[index] = newPosition;
             }
-            newP[index] = newPosition;
-            dfs(newP, newScore, depth + 1);
+            dfs(newP, score + newScore, depth + 1);
         }
     }
 
     private static int calculatePosition(int position, int dice) {
-        if (position > 20) {
-            if (position <= 23) {
-                if (position == 21 && dice == 1) {
-                    position = 22;
-                } else if (position == 21 && dice == 2) {
-                    position = 23;
-                } else if (position == 22 && dice == 1) {
-                    position = 23;
-                } else {
-                    position += 5 + dice;
-                }
-            } else if (position <= 25) {
-                if (position == 24 && dice == 1) {
-                    position = 25;
-                } else {
-                    position += 3 + dice;
-                }
-            } else {
-                position += dice;
-            }
-        } else if (position == 5) {
-            if (1 <= dice && dice <= 3) {
-                position = 20 + dice;
-            } else {
-                position = 25 + dice;
-            }
-        } else if (position == 10) {
-            if (1 <= dice && dice <= 2) {
-                position = 23 + dice;
-            } else {
-                position = 26 + dice;
-            }
-        } else if (position == 15) {
-            position = 25 + dice;
-        } else {
-            position += dice;
-            if (position > 20) {
-                position = 35;
-            }
+        int nextPosition = position + dice;
+        if (nextPosition == 5) {
+            nextPosition = 26;
+        } else if (nextPosition == 10) {
+            nextPosition = 41;
+        } else if (nextPosition == 15) {
+            nextPosition = 56;
         }
-        return position;
+        if (path[nextPosition] == 0) {
+            nextPosition = -1;
+        }
+        return nextPosition;
     }
 
     private static int indexOf(Integer[] p, int key) {
