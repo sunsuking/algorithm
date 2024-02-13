95fb3626577f24bb79bd97496f1f81e23b97c89b
sunsuking <sunsuking@gmail.com>
2024-02-13 13:36:32 +0900

    [Silver V] Title: 스네이크버드, Time: 88 ms, Memory: 11844 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(st.nextToken());
+        int[] nums = new int[num];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < num; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < num; i++) if (nums[i] <= length) length++;
+        System.out.println(length);
+    }
+}
