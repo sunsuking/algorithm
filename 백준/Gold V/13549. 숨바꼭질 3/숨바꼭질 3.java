26fe730d20c980d325c041d7bc497009612e4935
sunsuking <sunsuking@gmail.com>
2024-02-15 14:21:55 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Gold V/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+        boolean[] v = new boolean[100001];
+        Deque<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{start, 0});
+
+        if (start >= end) {
+            System.out.println(start - end);
+            System.exit(0);
+        }
+
+        while (!queue.isEmpty()) {
+            int[] pop = queue.poll();
+            if (v[pop[0]]) continue;
+
+            if (pop[0] == end) {
+                System.out.println(pop[1]);
+                System.exit(0);
+            }
+
+            v[pop[0]] = true;
+
+            if (pop[0] * 2 <= 100000) {
+                queue.push(new int[]{pop[0] * 2, pop[1]});
+            }
+            if (pop[0] + 1 <= 100000) {
+                queue.offer(new int[]{pop[0] + 1, pop[1] + 1});
+            }
+            if (pop[0] - 1 > 0) {
+                queue.offer(new int[]{pop[0] - 1, pop[1] + 1});
+            }
+        }
+    }
+}
