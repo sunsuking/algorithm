277761ad4aa54dda61c05944e23637cb468008ef
sunsuking <sunsuking@gmail.com>
2024-02-21 09:15:02 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 104 ms, Memory: 19504 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        long[] nums = new long[1_000_001];
+        nums[1] = 1;
+        nums[2] = 2;
+        nums[3] = 4;
+        for (int i = 4; i <= 1_000_000; i++) {
+            nums[i] = (nums[i - 1] + nums[i - 2]+ nums[i - 3]) % 1_000_000_009;
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            builder.append(nums[Integer.parseInt(br.readLine())]).append("\n");
+        }
+        System.out.print(builder);
+    }
+}
