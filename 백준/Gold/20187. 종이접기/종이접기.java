53af845586d9e8b07001212a5f259e064ff6578d
sunsuking <sunsuking@gmail.com>
2024-02-25 22:25:55 +0900

    [Gold III] Title: 종이접기, Time: 120 ms, Memory: 13604 KB, Score: 63 point -BaekjoonHub

--- 백준/Gold/20187. 종이접기/종이접기.java
+++ 백준/Gold/20187. 종이접기/종이접기.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 // D ->
 public class Main {
     private static char[] chars;
-    private static int funk, K, startX, startY;
+    private static int funk, K, startX, startY, size;
     private static int[][] squares;
 
     public static void main(String[] args) throws IOException {
@@ -17,8 +17,9 @@ public class Main {
         K = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         chars = new char[K * 2];
-        startX = K * 2;
-        startY = K * 2;
+        size = (int) Math.pow(2, K);
+        startX = size;
+        startY = size;
         for (int i = (K * 2) - 1; i >= 0; i--) {
             char c = st.nextToken().charAt(0);
             chars[i] = c;
@@ -28,15 +29,62 @@ public class Main {
                 startX /= 2;
             }
         }
-        startX--;
-        startY--;
+        startX = Math.max(startX - 1, 0);
+        startY = Math.max(startY - 1, 0);
         funk = Integer.parseInt(br.readLine());
-        squares = new int[K * 2][K * 2];
+        squares = new int[size][size];
         squares[startX][startY] = funk;
+
         dfs(1, 1, 0);
+        int n0 = 0;
+        int n1 = 0;
+        int n2 = 0;
+        int n3 = 0;
+        if (startX % 2 == 0) {
+            if (startY % 2 == 0) {
+                n0 = squares[startX][startY];
+                n1 = squares[startX][startY + 1];
+                n2 = squares[startX + 1][startY];
+                n3 = squares[startX + 1][startY + 1];
+            } else {
+                n0 = squares[startX][startY - 1];
+                n1 = squares[startX][startY];
+                n2 = squares[startX + 1][startY - 1];
+                n3 = squares[startX + 1][startY];
+            }
+        } else {
+            if (startY % 2 == 0) {
+                n0 = squares[startX - 1][startY];
+                n1 = squares[startX - 1][startY + 1];
+                n2 = squares[startX][startY];
+                n3 = squares[startX][startY + 1];
+            } else {
+                n0 = squares[startX - 1][startY - 1];
+                n1 = squares[startX - 1][startY];
+                n2 = squares[startX][startY - 1];
+                n3 = squares[startX][startY];
+            }
+        }
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                if (x % 2 == 0) {
+                    if (y % 2 == 0) {
+                        squares[x][y] = n0;
+                    } else {
+                        squares[x][y] = n1;
+                    }
+                } else {
+                    if (y % 2 == 0) {
+                        squares[x][y] = n2;
+                    } else {
+                        squares[x][y] = n3;
+                    }
+                }
+            }
+        }
         StringBuilder builder = new StringBuilder();
-        for (int x = 0; x < K * 2; x++) {
-            for (int y = 0; y < K * 2; y++) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
                 builder.append(squares[x][y]).append(" ");
             }
             builder.append("\n");
@@ -45,7 +93,7 @@ public class Main {
     }
 
     private static void dfs(int width, int height, int count) {
-        if (count == K * 2) return;
+        if ((count == K * 2)) return;
         char command = chars[count];
         if (command == 'L' || command == 'R') {
             width *= 2;
@@ -83,24 +131,6 @@ public class Main {
                         }
                     }
                 }
-            } else {
-                if (command == 'L') {
-                    int newStartX = startX - (startX % 2 == 0 ? 0 : 1);
-                    int newStartY = startY - (startY % 2 == 0 ? 0 : 1);
-                    for (int x = 0; x < height; x++) {
-                        for (int y = 0; y < width / 2; y++) {
-                            squares[newStartX + x][newStartY + y] = squares[newStartX + x][newStartY + y - (width * 2)];
-                        }
-                    }
-                } else {
-                    int newStartX = startX + (startX % 2 == 0 ? 2 : 1) - height;
-                    int newStartY = startY + (startY % 2 == 0 ? 2 : 1) - width;
-                    for (int x = 0; x < height; x++) {
-                        for (int y = 0; y < width / 2; y++) {
-                            squares[newStartX + x][newStartY + y] = squares[newStartX + x][newStartY + y + (width / 2)];
-                        }
-                    }
-                }
             }
         } else {
             height *= 2;
@@ -135,24 +165,6 @@ public class Main {
                             squares[startX - 1][startY + 1] = squares[startX][startY + 1] == 2 ? 0 : 1;
                         } else {
                             squares[startX - 1][startY - 1] = squares[startX][startY - 1] == 2 ? 0 : 1;
-                        }
-                    }
-                }
-            } else {
-                if (command == 'U') {
-                    int newStartX = startX - (startX % 2 == 0 ? 0 : 1);
-                    int newStartY = startY - (startY % 2 == 0 ? 0 : 1);
-                    for (int x = 0; x < height / 2; x++) {
-                        for (int y = 0; y < width; y++) {
-                            squares[newStartX + x][newStartY + y] = squares[newStartX + x - (height / 2)][newStartY + y];
-                        }
-                    }
-                } else {
-                    int newStartX = startX + (startX % 2 == 0 ? 2 : 1) - height;
-                    int newStartY = startY + (startY % 2 == 0 ? 2 : 1) - width;
-                    for (int x = 0; x < height / 2; x++) {
-                        for (int y = 0; y < width; y++) {
-                            squares[newStartX + x][newStartY + y] = squares[newStartX + x + (height / 2)][newStartY + y];
                         }
                     }
                 }
