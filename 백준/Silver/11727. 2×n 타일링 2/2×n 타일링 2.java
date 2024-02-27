277de83bc406278eb76ab699d55468298f386cb5
sunsuking <sunsuking@gmail.com>
2024-02-27 13:25:12 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 80 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+        for (int i = 3; i <= 1000; i++) {
+            dp[i] = ((dp[i - 1] % 10007) + (dp[i - 2] * 2 % 10007)) % 10007;
+        }
+        System.out.println(dp[num]);
+    }
+}
