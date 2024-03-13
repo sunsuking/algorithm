eff101c9a0559f06b559333a23e86d976b4ade32
sunsuking <sunsuking@gmail.com>
2024-03-13 15:26:28 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    private static int[] parent;
+    public int solution(int n, int[][] computers) {
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (computers[i][j] == 1) {
+                    merge(i + 1, j + 1);
+                }
+            }
+        }
+        
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i <= n; i++) {
+            set.add(find(parent[i]));
+        }
+        
+        return set.size();
+    }
+    
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    
+    private static boolean merge(int x, int y) {
+        int parentX = find(x);
+        int parentY = find(y);
+        if (parentX == parentY) return false;
+        parent[parentY] = parentX;
+        return true;
+    }
+}
