7cad24c8d069512bc7ebe7edf7de1c62a1a62d6b
sunsuking <sunsuking@gmail.com>
2024-04-02 11:59:28 +0900

    [Gold II] Title: 피보나치 수 3, Time: 100 ms, Memory: 17580 KB -BaekjoonHub

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -1,23 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		
-		int pisano = 1500000;
-		long size = Long.parseLong(br.readLine()) % pisano;
-		
-		long[] num = new long[pisano + 1];
-		
-		num[0] = 0;
-		num[1] = 1;
-		
-		for(int i = 2; i <= pisano; i++) {
-			num[i] = (num[i - 1] + num[i - 2]) % 1000000;
-		}
-		
-		System.out.print(num[(int)size]);
-	}
+    private static final int MOD = 1_000_000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger integer = new BigInteger(br.readLine());
+        int num = MOD / 10 * 15;
+        int[] dp = new int[num + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        int size = integer.mod(BigInteger.valueOf(num)).intValue();
+
+        System.out.println(dp[size]);
+    }
 }
