984e162d27d0ef3fd338d99f0df7cbfc9c741b50
sunsuking <sunsuking@gmail.com>
2024-04-22 11:20:34 +0900

    [Gold II] Title: 통나무 옮기기, Time: 100 ms, Memory: 13056 KB -BaekjoonHub

--- 백준/Gold/1938. 통나무 옮기기/통나무 옮기기.java
+++ 백준/Gold/1938. 통나무 옮기기/통나무 옮기기.java
@@ -1,201 +1,196 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
+
+/**
+ * 통나무 옮기기 G2
+ *  단순 구현 
+ *
+ */
+
 public class Main {
-	private static int num;
-	private static char[][] squares;
-	private static boolean[][][] visited;
-	private static int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+	
+	static int N;
+	static int[] moveX = {-1, 1, 0, 0};
+	static int[] moveY = {0, 0, -1, 1};
+	static int[][] board;
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		num = Integer.parseInt(br.readLine());
-		squares = new char[num][num];
-		List<int[]> positions = new ArrayList<>();
-		List<int[]> ends = new ArrayList<>();
-		for (int x = 0; x < num; x++) {
-			char[] chars = br.readLine().toCharArray();
-			for (int y = 0; y < num; y++) {
-				squares[x][y] = chars[y];
-				if (squares[x][y] == 'B') {
-					positions.add(new int[] {x, y});
-					squares[x][y] = '0';
-				} else if (squares[x][y] == 'E') {
-					ends.add(new int[] {x, y});
-					squares[x][y] = '0';
+		
+		N = Integer.parseInt(br.readLine());
+		
+		
+		board = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < N; j++) {
+				char tmp = input.charAt(j);
+				if (tmp == 'B') {
+					board[i][j] = -2;
+				}else if (tmp == 'E') {
+					board[i][j] = -1;
+				} else {
+					board[i][j] = tmp - '0';
 				}
 			}
 		}
-		
-		Train start = Train.newInstance(positions);
-		Train end = Train.newInstance(ends);
-		
-		Queue<Train> queue = new LinkedList<>();
-		queue.offer(start);
-		visited = new boolean[2][num][num];
-		while (!queue.isEmpty()) {
-			Train pop = queue.poll();
-			
-			if (pop.centerX == end.centerX && pop.centerY == end.centerY) {
-				System.out.println(pop.count);
-				System.exit(0);
-			}
-			
-			for (int i = 0; i < directions.length; i++) {
-				int newX = pop.centerX + directions[i][0];
-				int newY = pop.centerY + directions[i][1];
-				int index = pop.isVertical ? 0 : 1;
+
+		Log log = null;
+		for (int i = 0 ; i < N; i++) {
+			for (int j = 0 ; j < N; j++) {
 				
-				if (isIn(newX, newY) && !visited[index][newX][newY] && pop.canMove(i)) {
-					visited[index][newX][newY] = true;
-					queue.offer(new Train(newX, newY, pop.isVertical, pop.count + 1));
+				
+				if (isIn(i,j-1) && isIn(i,j+1) &&  board[i][j-1] == -2 && board[i][j] == -2 && board[i][j+1] == -2) {
+					log = new Log(i, j, 0, 0);
+					board[i][j] = 0;
+					board[i][j-1] = 0;
+					board[i][j+1] = 0;
+					
+					break;
+				}
+				
+				if (isIn(i-1,j) && isIn(i+1,j) &&  board[i-1][j] == -2 && board[i][j] == -2 && board[i+1][j] == -2) {
+					log = new Log(i, j, 1, 0);
+					board[i+1][j] = 0;
+					board[i-1][j] = 0;
+					board[i][j] = 0;
+					break;
+				}
+			}
+		}
+		
+		
+		
+		boolean[][][] visited = new boolean[2][N][N];
+		Queue<Log> q = new LinkedList<>();
+		
+		q.add(log);
+		visited[log.isUp][log.y][log.x] = true;
+		
+		
+		while(!q.isEmpty()) {
+			
+			
+			Log cur = q.poll();
+			
+			
+			if (cur.isUp == 1) {
+				
+				if (board[cur.y][cur.x]== -1
+						&& board[cur.y-1][cur.x]== -1
+						&& board[cur.y+1][cur.x]== -1) {
+					System.out.println(cur.cnt);
+					System.exit(0);
 				}
 			}
 			
-			if (pop.canRotate() && !visited[pop.isVertical ? 1 : 0][pop.centerX][pop.centerY]) {
-				visited[pop.isVertical ? 1 : 0][pop.centerX][pop.centerY] = true;
-				queue.offer(new Train(pop.centerX, pop.centerY, !pop.isVertical, pop.count + 1));
+			
+			
+			
+			if (cur.isUp == 0) {
+				
+				if (board[cur.y][cur.x]== -1
+						&& board[cur.y][cur.x-1]== -1
+						&& board[cur.y][cur.x+1]== -1) {
+					System.out.println(cur.cnt);
+					System.exit(0);
+				}
+			}
+			
+			
+			
+			for (int i = 0; i < 4; i++) {
+				int ny = cur.y + moveY[i];
+				int nx = cur.x + moveX[i];
+				
+				
+				if (cur.isUp == 1) {
+					
+					if (!isIn(ny + 1, nx) || !isIn(ny-1, nx)) continue;
+					if (board[ny-1][nx] == 1 || board[ny][nx] == 1 || board[ny+1][nx] == 1) continue;
+					if (visited[1][ny][nx]) continue;
+					
+					q.add(new Log(ny,nx,1,cur.cnt+1));
+					visited[1][ny][nx] = true;
+					
+				}else if(cur.isUp == 0){
+					
+					if (!isIn(ny, nx + 1) || !isIn(ny, nx-1)) continue;
+					if (board[ny][nx-1] == 1 || board[ny][nx] == 1 || board[ny][nx+1] == 1) continue;
+					if (visited[0][ny][nx]) continue;
+					
+					q.add(new Log(ny,nx,0,cur.cnt+1));
+					visited[0][ny][nx] = true;
+				}
+			}
+			
+			
+			int next = (cur.isUp + 1) % 2;
+			if (cur.turnPos() && !visited[next][cur.y][cur.x]) {
+
+				q.add(new Log(cur.y, cur.x, next, cur.cnt+1));
+				visited[next][cur.y][cur.x] = true;
 			}
 		}
+		
+		
 		System.out.println(0);
+		
 	}
 	
-	static class Train {
-		int centerX;
-		int centerY;
-		int count;
-		boolean isVertical;
+	
+	
+	
+	
+	
+	static class Log{
+		int y, x, isUp, cnt;
+
 		
-		public Train(int centerX, int centerY, boolean isVertical, int count) {
-			this.centerX = centerX;
-			this.centerY = centerY;
-			this.isVertical = isVertical;
-			this.count = count;
+		
+		
+		
+		
+		public Log(int y, int x, int isUp, int cnt) {
+			this.y = y;
+			this.x = x;
+			this.isUp = isUp;
+			this.cnt = cnt;
 		}
-		
-		public boolean canMove(int index) {
-			if (index == 0) {
-				return this.canRight();
-			} else if (index == 1) {
-				return this.canLeft();
-			} else if (index == 2) {
-				return this.canDown();
-			}
-			return this.canUp();
-		}
-		
-		public boolean canRotate() {
-			for (int x = centerX - 1; x <= centerX + 1; x++) {
-				for (int y = centerY - 1; y <= centerY + 1; y++) {
-					if (!isIn(x, y) || squares[x][y] != '0') {
-						return false;
-					}
+
+
+
+		public boolean turnPos() {
+			
+			for (int i = y-1; y + 1 >= i; i++) {
+				for (int j = x - 1; x + 1 >= j; j++) {
+					if (!isIn(i, j) || board[i][j] == 1) return false;
 				}
 			}
 			return true;
 		}
-		
-		private boolean canUp() {
-			if (isVertical) {
-				for (int x = centerX - 1; x <= centerX + 1; x++) {
-					if (!isIn(x - 1, centerY) || squares[x - 1][centerY] != '0') {
-						return false;
-					}
-				}
-				return true;
-			} else {
-				for (int y = centerY - 1; y <= centerY + 1; y++) {
-					if (!isIn(centerX - 1, y) || squares[centerX - 1][y] != '0') {
-						return false;
-					}
-				}
-				return true;
-			}
-		}
-		
-		private boolean canDown() {
-			if (isVertical) {
-				for (int x = centerX - 1; x <= centerX + 1; x++) {
-					if (!isIn(x + 1, centerY) || squares[x + 1][centerY] != '0') {
-						return false;
-					}
-				}
-				return true;
-			} else {
-				for (int y = centerY - 1; y <= centerY + 1; y++) {
-					if (!isIn(centerX + 1, y) || squares[centerX + 1][y] != '0') {
-						return false;
-					}
-				}
-				return true;
-			}
-		}
-		
-		private boolean canLeft() {
-			if (isVertical) {
-				for (int x = centerX - 1; x <= centerX + 1; x++) {
-					if (!isIn(x, centerY - 1) || squares[x][centerY - 1] != '0') {
-						return false;
-					}
-				}
-				return true;
-			} else {
-				for (int y = centerY - 1; y <= centerY + 1; y++) {
-					if (!isIn(centerX, y - 1) || squares[centerX][y - 1] != '0') {
-						return false;
-					}
-				}
-				return true;
-			}
-		}
-		
-		private boolean canRight() {
-			if (isVertical) {
-				for (int x = centerX - 1; x <= centerX + 1; x++) {
-					if (!isIn(x, centerY + 1) || squares[x][centerY + 1] != '0') {
-						return false;
-					}
-				}
-				return true;
-			} else {
-				for (int y = centerY - 1; y <= centerY + 1; y++) {
-					if (!isIn(centerX, y + 1) || squares[centerX][y + 1] != '0') {
-						return false;
-					}
-				}
-				return true;
-			}
-		}
-		
-		public static Train newInstance(List<int[]> positions) {
-			int centerX = 0;
-			int centerY = 0;
-			for (int i = 0; i < 3; i++) {
-				centerX += positions.get(i)[0];
-				centerY += positions.get(i)[1];
-			}
-			centerX /= 3;
-			centerY /= 3;
-			
-			boolean isVertical = positions.get(0)[0] != centerX;
-			return new Train(centerX, centerY, isVertical, 0);
-		}
+
+
 
 		@Override
 		public String toString() {
-			return "Train [centerX=" + centerX + ", centerY=" + centerY + ", count=" + count + ", isVertical="
-					+ isVertical + "]";
+			return "Log [y=" + y + ", x=" + x + ", isUp=" + isUp + ", cnt=" + cnt + "]";
 		}
+
+
+
 		
 		
 	}
 	
-	private static boolean isIn(int x, int y) {
-		return x >= 0 && y >= 0 && x < num && y < num;
+	
+	static boolean isIn(int y, int x) {
+		return 0 <= y && y < N && 0 <= x && x < N;
 	}
 }
